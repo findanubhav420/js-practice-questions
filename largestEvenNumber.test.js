49b9e7690d3e720a03b271d7132f88622b3e55a2
findanubhav420 <findanubhav420@gmail.com>
2023-01-14 16:57:53 +0530

    feat: add test for function to check largest even number

--- largestEvenNumber.test.js
+++ largestEvenNumber.test.js
@@ -4,3 +4,7 @@ test('It is getting largest Even Number',()=>{
     let numbers=[1, 2, 3, 4, 5]
     expect(largestEvenNumber(numbers)).toBe(4)
 })
+test('It is getting largest Even Number',()=>{
+    let numbers=[1, 3, 5, 7]
+    expect(largestEvenNumber(numbers)).toBe(-1)
+})
