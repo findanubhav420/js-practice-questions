31dc35b33bb5dfb15fddc514c63a6f232d9a9d45
findanubhav420 <findanubhav420@gmail.com>
2023-01-14 16:50:06 +0530

    feat: add function to check largest even number

---
+++ largestEvenNumber.js
@@ -0,0 +1,14 @@
+function largestEvenNumber(numbers){
+    let res=-1;
+    // numbers.forEach(LargestEvenNumber);
+
+    // function LargestEvenNumber(number){
+    //     if(number%2===0&& number>res ) res == number
+    // }
+    for (let index=0;index<numbers.length;index++)
+    if(numbers[index]%2===0 && numbers[index]>res )
+        res =numbers[index]
+    return res;
+}
+
+module.exports= largestEvenNumber;
