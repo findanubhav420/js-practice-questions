541bb050a6030590a9f6e591ddaa2d0b26c491b3
findanubhav420 <findanubhav420@gmail.com>
2023-01-15 08:11:15 +0530

    feat: function to change case of the string

---
+++ caseChanger.js
@@ -0,0 +1,17 @@
+function caseChanger(str){
+    var newstr= '';
+    let len=str.length;
+    for(let index=0; index<len;index++){
+        if(str.charCodeAt(index)>=97&&str.charCodeAt(index)<=122){
+            newstr+=str[index].toUpperCase();
+        }
+        else if(str.charCodeAt(index)>=65&&str.charCodeAt(index)<=90){
+            newstr+=str[index].toLowerCase();
+        }
+            else
+                newstr+=str[index];
+    }
+    return newstr;
+
+}
+module.exports =caseChanger;
