bd55eeba90cf67f480dca8f86216e1da6f1b7410
findanubhav420 <findanubhav420@gmail.com>
2023-01-15 08:20:23 +0530

    feat: add function to check repdigit

--- digitChecker.js
+++ digitChecker.js
@@ -16,4 +16,22 @@ function digitChecker(number){
     return true;
 }
 
+const digitCheckerArrow=(number)=>{
+    let last_digit=number%10;
+    
+    while(number!=0){
+
+        // number=number/10;
+        
+        let current_digit=number%10
+        number=(number/10);
+        if(last_digit!=current_digit)
+        {
+            return false;
+        }
+
+    }
+    return true;
+}
+
 module.exports =digitChecker;
