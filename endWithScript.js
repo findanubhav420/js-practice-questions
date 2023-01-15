147b26fe551a86dc1a69a8a6619d3139f3ae7975
findanubhav420 <findanubhav420@gmail.com>
2023-01-15 09:18:59 +0530

    feat: add function to check strings end with Script

---
+++ endWithScript.js
@@ -0,0 +1,19 @@
+function endWithScript(str){
+    if(str.lenth<6)
+    return false;
+
+    let result= str.slice(str.length-6,str.length);
+    if(result=='Script')
+    return true;
+    else return false;
+}
+const endWithScriptArrow = ()=>{
+    if(str.lenth<6)
+    return false;
+
+    let result= str.slice(str.length-6,str.length);
+    if(result=='Script')
+    return true;
+    else return false;
+}
+module.exports=endWithScript;
