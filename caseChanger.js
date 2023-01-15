543f2cd9f8762630c94342f72f8ab43354ebace3
findanubhav420 <findanubhav420@gmail.com>
2023-01-15 08:17:30 +0530

    feat: function to change case of the string

--- caseChanger.js
+++ caseChanger.js
@@ -1,21 +1,21 @@
-// function caseChanger(str){
-//     var newstr= '';
-//     let len=str.length;
-//     for(let index=0; index<len;index++){
-//         if(str.charCodeAt(index)>=97&&str.charCodeAt(index)<=122){
-//             newstr+=str[index].toUpperCase();
-//         }
-//         else if(str.charCodeAt(index)>=65&&str.charCodeAt(index)<=90){
-//             newstr+=str[index].toLowerCase();
-//         }
-//             else
-//                 newstr+=str[index];
-//     }
-//     return newstr;
+function caseChanger(str){
+    var newstr= '';
+    let len=str.length;
+    for(let index=0; index<len;index++){
+        if(str.charCodeAt(index)>=97&&str.charCodeAt(index)<=122){
+            newstr+=str[index].toUpperCase();
+        }
+        else if(str.charCodeAt(index)>=65&&str.charCodeAt(index)<=90){
+            newstr+=str[index].toLowerCase();
+        }
+            else
+                newstr+=str[index];
+    }
+    return newstr;
 
-// }
+}
 
-const caseChanger =(str)=>{
+const caseChangerArrow =(str)=>{
     var newstr= '';
     let len=str.length;
     for(let index=0; index<len;index++){
